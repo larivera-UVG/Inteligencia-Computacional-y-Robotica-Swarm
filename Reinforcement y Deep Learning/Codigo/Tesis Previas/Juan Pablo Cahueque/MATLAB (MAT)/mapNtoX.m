91f7ac7507a6888898580cad53130f7517627320
Gaby <iri16009@uvg.edu.gt>
2020-06-16 19:54:09 -0600

    Eliminando más cosas
    salu2

--- Reinforcement y Deep Learning/Codigo/Tesis Previas/Juan Pablo Cahueque/MATLAB (MAT)/mapNtoX.m
+++
@@ -1,7 +0,0 @@
-function [ real_coord ] = mapNtoX(pos,gridsize,step )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-    real_coord = pos.*(gridsize/step)-gridsize/2;
-
-end
-
--- Reinforcement y Deep Learning/Codigo/Tesis Previas/Juan Pablo Cahueque/MATLAB (MAT)/mapXtoN.m
+++
@@ -1,7 +0,0 @@
-function [ mapped_pos ] = mapXtoN( pos,gridsize,step )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-    mapped_pos = [round((pos(1)+gridsize/2)*(step/gridsize)),round((pos(2)+gridsize/2)*(step/gridsize))];
-
-end
-
