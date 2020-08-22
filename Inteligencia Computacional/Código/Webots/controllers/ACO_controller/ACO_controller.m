692f0ecc0466bbad4a1d9fc94dcbf6057d44f75d
iri16009 <iri16009@uvg.edu.gt>
2020-08-22 16:24:31 -0600

    Funcionan 2 controladores!!
    Los dos PID ya funcionan. Están comentados para no arruinarlos xdxd

--- Inteligencia Computacional/Código/Webots/controllers/ACO_controller/ACO_controller.m
+++ Inteligencia Computacional/Código/Webots/controllers/ACO_controller/ACO_controller.m
@@ -7,8 +7,8 @@
 
 % uncomment the next two lines if you want to use
 % MATLAB's desktop to interact with the controller:
-desktop;
-%keyboard;
+% desktop;
+% keyboard;
 
 TIME_STEP = 32;
 
@@ -17,14 +17,15 @@ TIME_STEP = 32;
 %  wb_camera_enable(camera, TIME_STEP);
 %  motor = wb_robot_get_device('motor');
 % control step
-TIME_STEP = 32;
-SPEED_UNIT = 0.00628;
-ell = 71/2000;  % Distance from center
-r = 20.5/1000;  % Radio de las llantas
+TIME_STEP = 32;  % milisegundos
+ell = 71/2000;  % Distance from center en metros
+r = 20.5/1000;  % Radio de las llantas en metros
 MAX_SPEED = 6.28;
-goal_points= [-5, -4]; % Este
-%goal_points= [-2, -4]; % Este
-% goal_points= [-4, -2]; % SUR
+goal = [0, 0];  % Diagonal
+goal = [-4, -5]; % Norte
+% goal = [4, -4];
+ goal = [-2, -4]; % Este
+% goal = [-4, -2]; % Sur
 
 %% Obtener todos los sensores del e-Puck
 
@@ -48,91 +49,145 @@ wb_compass_enable(orientation_sensor, TIME_STEP);
 % wb_receiver_enable(receiver,TIME_STEP);
 
 %% Valores iniciales
-xg = goal_points(1);
-zg = goal_points(2);
+xg = goal(1);  zg = goal(2);
 step = 0;
-angle = 0;
-alpha = 0.9;
-epsilon = 0.1;
+epsilon = 0.05;
 
 %% Variables PID
-ex = 0;
-ez = 0;
-ep = 0;
-theta_g = 0;
-eo = 0;
-v = 0;
+% Acercamiento exponencial
+alpha = 0.9;
 
-e_k_1 = 0;
-E_k = 0;
-e_k = 0;
-eD = 0;
-u_k = 0;
+% PID de orientación
+eO_k_1 = 0; % Error derivativo
+EO_k = 0;   % Error integral
+
+% PID de posición
+eP_1 = 0;
+EP = 0;
 
 % CONSTANTES DEL PID
-kP = 8;
-kD = 1;
-kI = 5;
+% 1 - Acercamiento exponencial
+% kP_O = 20;
+% kD_O = 5;
+% kI_O = 9;
 
-speed = zeros(1,2);
-state = "f";
-controlador = 1;
-left_speed = 0;
-right_speed = 0;
+% 2 - Velocidad lineal y angular
+% kP_O2 = 1;
+% kD_O2 = 100; 
+% kI_O2 = 40;
+
+% kP_P = 2;
+% kD_P = 0;
+% kI_P = 0.0001;
+
+% 3 - Control de pose
+k_rho = 0.1;
+k_alpha = 50;
+k_beta = -25; % Mejor: -25
+
+% k_rho = 0.1;
+% k_alpha = 0.5;
+% k_beta = -25; % Mejor: -25
+
+controlador = 3;
+% controlador
+% 0 - OFF
+% 1 - PID de acercamiento exponencial
+% 2 - PID
+% 3 - Pose
+
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
 while wb_robot_step(TIME_STEP) ~= -1
-
-  step = step + 1;
-  
-  pos = wb_gps_get_values(position_sensor);
-  xi = pos(1);
-  zi = pos(3);  
-  
-  north = wb_compass_get_values(orientation_sensor);
-  rad = atan2(north(1), north(3));
-  angle = rad + pi;
-  
-   if (xi >= xg - epsilon) && (xi <= xg + epsilon) && (zi >= zg - epsilon) && (zi <= zg + epsilon)
+    
+    step = step + 1;
+    
+    pos = wb_gps_get_values(position_sensor);
+    xi = pos(1);  zi = pos(3);
+    
+    north = wb_compass_get_values(orientation_sensor);
+    rad = atan2(north(1), north(3));
+    theta = rad + pi;  % Se corrige el ángulo para que esté igual que theta_g
+    
+    if (xi >= xg - epsilon) && (xi <= xg + epsilon) && (zi >= zg - epsilon) && (zi <= zg + epsilon)
         controlador = 0;
     end
     
-    % ------------- OFF ------------- 
+    % ------------- OFF -------------
     if controlador == 0
-        left_speed = 0;
-        right_speed = 0;
- 
-    % ------------- PID ------------- 
-    elseif controlador == 1
-        % Error de posicion
-        ex = xg - xi;
-        ez = zg - zi;
-        ep = sqrt(ex*ex + ez*ez);  % error total de posicion
-        theta_g = atan2(ez, ex);
-
-        % Error de orientacion
-        eo = atan2(sin(theta_g - angle), cos(theta_g - angle));
-        e_k = eo;
+        speed = [0, 0];
         
-        disp(north)
-        formatSpec = 'xi: %.2f, zi: %.2f ep: %.2f | theta: %.2f theta g: %.2f eo: %.2f \n';
-        fprintf(formatSpec, xi, zi, ep, angle, theta_g, eo);
+    % ------------- PID exp ---------
+    elseif controlador == 1
+        % Error total de posicion
+        eP = sqrt((xg - xi)^2 + (zg - zi)^2);
+        
+        % Error de orientacion
+        theta_g = atan2((zg - zi), (xg - xi));
+        eO = atan2(sin(theta_g - theta), cos(theta_g - theta));
+        
         
         % Control de velocidad angular
-        eD = e_k - e_k_1;  % error derivat
-        E_k = E_k + e_k;  % error acumulado
-        u_k = kP*e_k + kI*E_k + kD*eD;  % salida del controlador
-        e_k_1 = e_k;  % actualizar las variables
+        eD = eO - eO_k_1;  % error derivativo
+        EO_k = EO_k + eO;  % error acumulado
+        w = kP_O*eO + kI_O*EO_k + kD_O*eD;
+        eO_k_1 = eO;  % actualizar las variables
         
+        v = MAX_SPEED*(1 - exp(-eP*eP*alpha))/eP;
         
-        v = MAX_SPEED*(1 - exp(-ep*ep*alpha))/ep;  % velocidad uniciclo
+        % velocidad uniciclo
+        left_speed = (v + w*ell)/r;
+        right_speed = (v - w*ell)/r;
+        speed = [left_speed, right_speed];
         
-        left_speed = (v + u_k*ell)/r;
-        right_speed = (v - u_k*ell)/r;
-   end
-    speed = [left_speed, right_speed];
+    elseif controlador == 2
+        % Error total de posicion
+        eP = sqrt((xg - xi)^2 + (zg - zi)^2);
+        
+        % Error de orientacion
+        theta_g = atan2((zg - zi), (xg - xi));
+        eO = atan2(sin(theta_g - theta), cos(theta_g - theta));
+        
+%         formatSpec = 'xi: %.2f, zi: %.2f eP: %.2f | theta: %.2f theta g: %.2f eO: %.2f \n';
+%         fprintf(formatSpec, xi, zi, eP, theta, theta_g, eO);
+
+        % Control de velocidad lineal
+        eP_D = eP - eP_1;
+        EP = EP + eP;
+        v = kP_P*eP + kI_P*EP + kD_P*eP_D;
+        eP_1 = eP;
+        
+        % Control de velocidad angular
+        eD = eO - eO_k_1;  % error derivativo
+        EO_k = EO_k + eO;  % error acumulado
+        w = kP_O2*eO + kI_O2*EO_k + kD_O2*eD;
+        eO_k_1 = eO;  % actualizar las variables
+        
+        % velocidad uniciclo
+        left_speed = (v + w*ell)/r;
+        right_speed = (v - w*ell)/r;
+        speed = [left_speed, right_speed];
+        
+    elseif controlador == 3
+        % Error total de posicion
+        rho = sqrt((xg - xi)^2 + (zg - zi)^2);
+        
+        % Error de orientacion
+        theta_g = atan2((zg - zi), (xg - xi));
+        alpha = -theta + theta_g;
+        beta = -theta - alpha;
+        v = k_rho*rho;
+        w = k_alpha*alpha + k_beta*beta;
+        
+        % velocidad uniciclo
+        left_speed = (v + w*ell)/r;
+        right_speed = (v - w*ell)/r;
+        speed = [left_speed, right_speed];
+    end
+    
+    % Truncamos la velocidad
     for k = 1:2
         if speed(k) < -MAX_SPEED
             speed(k) = -MAX_SPEED;
@@ -140,14 +195,10 @@ while wb_robot_step(TIME_STEP) ~= -1
             speed(k) = MAX_SPEED;
         end
     end
-   % speed = [0, 0];
-    left_speed = speed(1);
-    right_speed = speed(2);
-%    right_speed = MAX_SPEED/4;
+    
+    left_speed = speed(1); right_speed = speed(2);
     wb_motor_set_velocity(left_motor, left_speed);
     wb_motor_set_velocity(right_motor, right_speed);
-         
-
 
 end
 
