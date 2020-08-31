a40543dae0d35841824f4b060eb2ffc88b35d5e1
eddysanoli <eddysanoli@gmail.com>
2020-08-30 21:33:16 -0600

    Controlador Closed-Loop Steering y Documentación
    Cambios:
    - Documentación actualizada para getControllerOutput. Ahora al escribir "help getControllerOutput" ya se mencionan todos los controladores implementados.
    - Último controlador de pose o de tipo NKC (Como lo definiría Aldo en su script de Webots). Aldo tuvo algunos problemas implementando este, entonces no se recomienda su uso. Funciona, pero es claro que requiere de retoques (El robot camina de reversa hacia la meta y tiembla). Incluído únicamente con el propósito de agregar todos los controladores de Aldo.

--- Reinforcement y Deep Learning/Codigo/Matlab/Eduardo Santizo/Funciones/Internas/getControllerOutput.m
+++ Reinforcement y Deep Learning/Codigo/Matlab/Eduardo Santizo/Funciones/Internas/getControllerOutput.m
@@ -29,15 +29,41 @@ function [VelLineal, VelAngular] = getControllerOutput(ControllerType, Meta, Puc
 %
 % Opciones "ControllerType"
 %   
-%   - PID: Control PID con filtro "hard stops".    
+%   - Pose Simple: Controlador propio del libro "Introduction to Autonomous
+%     Mobile Robots" de Roland Siegwart. Específicamente explicado en la
+%     sección 3.6. Este hace uso de tres ángulos: Alpha (Ángulo entre la
+%     línea de orientación del robot y la línea que se dibuja del robot a
+%     la meta), Beta (Ángulo entre la línea que se dibuja del robot a la
+%     meta y la horizontal del marco de referencia global / el eje X de
+%     matlab) y Rho_p (La distancia entre la meta y el centroide del
+%     robot). Utilizando las constantes K_Rho, K_Alpha y K_Beta, se calcula
+%     la velocidad angular (w = K_Alpha * Alpha + K_Beta * Beta) y lineal 
+%     (v = K_Rho * Rho_p).
 %
-%   - TUC-LQR: Control LQR diseñado utilizando la función lqr(A,B,Q,R) de 
+%   - Lyapunov: Controlador similar al controlador de pose simple de
+%     Siegwart. La diferencia es que este intenta asegurar que el estado
+%     del sistema converga asintóticamente a (Rho_p, Alpha, Beta) = [0 0
+%     Beta]. Para asegurar dicha convergencia hace uso del criterio de
+%     estabilidad de Lyapunov. Para esto, se hace uso de una ecuación de
+%     energía interna de lyapunov con la forma V = 0.5*Rho_p^2 +
+%     0.5*Alpha^2. Luego de los cálculos respectivos (Nadalini, pág. 31),
+%     las reglas de actualización resultantes son v = K_Rho * Rho_p *
+%     cos(Alpha) y w = K_Rho * sin(Alpha) * cos(Alpha) + K_Alpha * Alpha.
+%
+%   - Closed-Loop Steering: Controlador basado en la investigación de Park
+%     y Kuipers en el paper "A Smooth control law for graceful motion of
+%     differential wheeled mobile robots in 2D environment". Aldo tuvo
+%     algunos problemas al implementarlo, entonces el controlador no
+%     funciona totalmente bien. Requiere de mejoras. No se recomienda
+%     utilizarlo.
+%
+%   - LQR: Control LQR diseñado utilizando la función lqr(A,B,Q,R) de 
 %     Matlab donde A = [0 0; 0 0], B = [1 0; 0 1], R = B y Q = B*0.01.
 %     Utilizando este controlador se observaron trayectorias casi rectas
 %     hacia la meta. No ventajoso para espacios de búsqueda con obstáculos.
 %     Parámetros adicionales: RadioCuerpoPuck
 %
-%   - TUC-LQI: Control LQI diseñado utilizando la función lqr(A,B,Q,R) de 
+%   - LQI: Control LQI diseñado utilizando la función lqr(A,B,Q,R) de 
 %     Matlab donde A = 0, B = I, C = I, Q = eye(4) y R = 2000 * eye(2). 
 %     Las matrices resultantes K y Ki fueron 0.2127 * eye(2) y -0.0224 *
 %     eye(2) respectivamente. Resultados casi idénticos a los del LQR, con
@@ -84,7 +110,7 @@ switch ControllerType
         % Beta = - Theta_o - Alpha (Nadalini, pág 29), pero Theta_o está 
         % entre 0 y 2pi luego de usar "wrapTo2Pi". Se le resta pi, para 
         % regresarlo entre [-pi, pi] y que así esté entre el mismo rango de
-        % Alpha.
+        % Alpha. Beta estará entre -pi y pi
         Theta_o = Theta_o - pi;
         Beta = angdiff(Theta_o, -Alpha);
         
@@ -129,6 +155,54 @@ switch ControllerType
         VelLineal = K_Rho .* Rho_p .* cos(Alpha);
         VelAngular = (K_Rho .* sin(Alpha) .* cos(Alpha)) + K_Alpha .* Alpha;
     
+    % Controlador por Closed-Loop Steering
+    % Controlador no recomendado. Aldo tuvo problemas al implementarlo.
+    case "Closed-Loop Steering"
+        K1 = 1;
+        K2 = 10;
+        
+        % Parámetros de controlador
+        K_Rho = 0.1;
+        
+        % Rho_p: Distancia de todos los Pucks a la Meta
+        [~,Rho_p] = dsearchn(Meta,PuckPosicion_Actual);
+        
+        % Error de posición
+        ErrorPos = Meta - PuckPosicion_Actual;
+        
+        % Ángulo de línea entre meta y robot (Theta goal)
+        % Para que todos los ángulos estén entre los mismos límites se
+        % mapean los valores de 0 a 2pi (wrapTo2Pi)
+        Theta_g = atan2(ErrorPos(:,2),ErrorPos(:,1));
+        Theta_g = wrapTo2Pi(Theta_g);
+        
+        % Orientación actual del robot
+        Theta_o = PuckOrientacion_Actual;
+        Theta_o = wrapTo2Pi(Theta_o);
+        
+        % Error de orientación
+        % Se utiliza "angdiff()" para calcular la diferencia entre ambos
+        % ángulos para tomar en cuenta que ángulos como "2pi" y "0" son
+        % virtualmente el mismo ángulo. Entonces, usando esta función, 
+        % la diferencia entre 6.27 (Casi 2pi) y 0.78 (Casi pi/4), será muy
+        % cercana a la diferencia entre 0 y 0.78 (Casi pi/4). Los ángulos 
+        % resultantes están acotados entre -pi y pi.
+        Alpha = angdiff(Theta_o, Theta_g);
+        
+        % Beta = - Theta_o - Alpha (Nadalini, pág 29), pero Theta_o está 
+        % entre 0 y 2pi luego de usar "wrapTo2Pi". Se le resta pi, para 
+        % regresarlo entre [-pi, pi] y que así esté entre el mismo rango de
+        % Alpha. Beta estará entre -pi y pi
+        Theta_o = Theta_o - pi;
+        Beta = angdiff(Theta_o, -Alpha);
+        
+        % Velocidad Lineal (Nadalini, pág. 31)
+        VelLineal = K_Rho .* Rho_p .* cos(Alpha);
+        
+        % Velocidad Angular (Nadalini, pág 33)
+        VelAngular = (2/5)*(VelLineal ./ Rho_p).*(K2*(Alpha + atan(-K1*Beta)) + ...
+                     (1 + (K1./(1 + (K1*Beta).^2))).*sin(Alpha));
+        
     % Controlador por Regulador Lineal Cuadrático (LQR)
     case "LQR"
         
